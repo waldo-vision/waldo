039e9230838e78eba3786ff1840aa31126a307f4
Husky <39809509+Huskydog9988@users.noreply.github.com>
2022-11-19 14:51:23 -0500

    feat(opentel): added basic tracing

---
+++ apps/backend/src/tracing.ts
@@ -0,0 +1,29 @@
+// Require dependencies
+import * as opentelemetry from '@opentelemetry/sdk-node';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+// import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// For troubleshooting, set the log level to DiagLogLevel.DEBUG
+// diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+
+const sdk = new opentelemetry.NodeSDK({
+  traceExporter: new OTLPTraceExporter({
+    // optional - default url is http://localhost:4318/v1/traces
+    url: `${
+      process.env.OTLP_COLLECTOR_BASE || 'http://localhost:4318'
+    }/v1/traces`,
+    // optional - collection of custom headers to be sent with each request, empty by default
+    headers: {},
+    // an optional limit on pending requests
+    concurrencyLimit: 10,
+  }),
+  // basically does every intrumentation for us, can fine tune later if needed
+  // https://opentelemetry.io/registry/?language=js&component=instrumentation
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+sdk.start();
