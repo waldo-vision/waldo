cde3df1631da7239c50bd7f5585f517487701721
Husky <39809509+Huskydog9988@users.noreply.github.com>
2022-11-21 13:58:07 -0500

    feat(next auth): add prisma

--- apps/web/pages/api/auth/[...nextauth].js
+++ apps/web/pages/api/auth/[...nextauth].js
@@ -1,8 +1,11 @@
 /* eslint-disable turbo/no-undeclared-env-vars */
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import DiscordProvider from 'next-auth/providers/discord';
+import { prisma } from '@utils/client';
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID,
