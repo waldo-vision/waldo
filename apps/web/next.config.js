13f66d2f657fd6dbf054b2558e1bbfedab6ae22a
Husky <39809509+Huskydog9988@users.noreply.github.com>
2022-11-25 18:38:03 -0500

    feat(docker): add basic docker support

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,18 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   swcMinify: true,
   experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../'),
     transpilePackages: ['ui'],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 };
 
