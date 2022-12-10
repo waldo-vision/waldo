c10bd655c31de4c2de284cebede9882004f0a5b8
Husky <39809509+Huskydog9988@users.noreply.github.com>
2022-12-10 18:16:14 -0500

    fix(trpc): clips router not exposed

--- apps/web/server/trpc/router/_app.ts
+++ apps/web/server/trpc/router/_app.ts
@@ -1,8 +1,10 @@
 import { router } from '../trpc';
+import { clipRouter } from './clip';
 import { gameplayRouter } from './gameplay';
 
 export const appRouter = router({
   gameplay: gameplayRouter,
+  clip: clipRouter,
 });
 
 // export type definition of API
