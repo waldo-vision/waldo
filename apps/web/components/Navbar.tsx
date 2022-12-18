986556f0efd36010422bce1e885b11f225815162
ceriddenn <90457772+ceriddenn@users.noreply.github.com>
2022-12-18 14:41:09 -0500

    add: turnstile base; add: turnstile on upload page

--- apps/web/components/Navbar.tsx
+++ apps/web/components/Navbar.tsx
@@ -13,7 +13,6 @@ import {
 import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-
 import WaldoLogo from '../public/android-chrome-256x256.png';
 import GithubLogo from '../public/navbar_github.png';
 
--- apps/web/server/trpc/router/util.ts
+++ apps/web/server/trpc/router/util.ts
@@ -31,4 +31,40 @@ export const utilRouter = router({
         });
       }
     }),
+  verifyUser: protectedProcedure
+    .meta({ openapi: { method: 'PUT', path: '/user/verify' } })
+    .input(
+      z.object({
+        tsToken: z.string(),
+      }),
+    )
+    .output(
+      z.object({
+        result: z.boolean(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const endpoint =
+        'https://challenges.cloudflare.com/turnstile/v0/siteverify';
+      const body = `secret=${encodeURIComponent(
+        process.env.CLOUDFLARE_TURNSTILE_SECRET,
+      )}&response=${encodeURIComponent(input.tsToken)}`;
+      const request = await fetch(endpoint, {
+        method: 'POST',
+        body,
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+        },
+      });
+      const result = await request.json();
+      if (result.success) {
+        return {
+          result: true,
+        };
+      } else {
+        return {
+          result: false,
+        };
+      }
+    }),
 });
