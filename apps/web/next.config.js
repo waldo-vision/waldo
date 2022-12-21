548270b3a90a3bab13bfe14b9b6104afb212bc44
ceriddenn <90457772+ceriddenn@users.noreply.github.com>
2022-12-21 17:40:57 -0500

    add: nextjs securityheaders

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
+const securityHeaders = [
+  { key: 'Strict-Transport-Security', value: 'max-age=63072000' },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  { key: 'X-XSS-Protection', value: '1; mode=block' },
+  { key: 'X-Frame-Options', value: 'deny' },
+];
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -17,6 +27,15 @@ const nextConfig = {
   i18n: {
     locales: ['en-US'],
     defaultLocale: 'en-US',
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
   },
 };
 
