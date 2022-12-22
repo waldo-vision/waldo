efe9a814f07bf8aff9b15ba48deafdc382adbf5b
Husky <39809509+Huskydog9988@users.noreply.github.com>
2022-12-22 18:26:22 -0500

    fix(web): user schema not in line with db

--- apps/web/utils/zod/dash.ts
+++ apps/web/utils/zod/dash.ts
@@ -2,10 +2,10 @@ import { z } from 'zod';
 
 export const UserSchema = z.object({
   id: z.string(),
-  name: z.string(),
-  email: z.string(),
+  name: z.string().nullable(),
+  email: z.string().nullable(),
   emailVerified: z.date().nullable(),
-  image: z.string(),
+  image: z.string().nullable(),
   blacklisted: z.boolean(),
   role: z.string(),
 });
