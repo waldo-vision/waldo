98535552adeb4ea6e8028fc66f928b5b3f4a1785
ceriddenn <90457772+ceriddenn@users.noreply.github.com>
2022-12-23 14:51:17 -0500

    status and user pages completely finished

--- apps/web/components/dashboard/status/Stats.tsx
+++ apps/web/components/dashboard/status/Stats.tsx
@@ -8,74 +8,121 @@ import {
   Flex,
 } from '@chakra-ui/react';
 import { FaCircle, FaExclamationCircle } from 'react-icons/fa';
-
+import { trpc } from '@utils/trpc';
 export default function Stats() {
-  return (
-    <StatGroup>
-      <Stat
-        bgColor={'white'}
-        borderRadius={16}
-        p={5}
-        m={5}
-        minW={{ base: '80%', md: '30%' }}
-      >
-        <StatLabel>Security & Authentication</StatLabel>
-        <StatNumber>Operational</StatNumber>
-        <StatHelpText>
-          <Flex
-            flexDirection={'row'}
-            align={'center'}
-            textAlign={'center'}
-            gap={2}
-          >
-            <FaCircle size={8} color={'green'} />
-            <Text>No current issues.</Text>
-          </Flex>
-        </StatHelpText>
-      </Stat>
-      <Stat
-        bgColor={'white'}
-        borderRadius={16}
-        p={5}
-        m={5}
-        minW={{ base: '80%', md: '30%' }}
-      >
-        <StatLabel>Data Collection</StatLabel>
-        <StatNumber>Operational</StatNumber>
-        <StatHelpText>
-          <Flex
-            flexDirection={'row'}
-            align={'center'}
-            textAlign={'center'}
-            gap={2}
-          >
-            <FaCircle size={8} color={'green'} />
-            <Text>No current issues.</Text>
-          </Flex>
-        </StatHelpText>
-      </Stat>
+  const { data: aData, isLoading: aLoading } = trpc.site.getPageData.useQuery({
+    name: 'account',
+  });
+  const { data: rData, isLoading: rLoading } = trpc.site.getPageData.useQuery({
+    name: 'review',
+  });
+  const { data: uData, isLoading: uLoading } = trpc.site.getPageData.useQuery({
+    name: 'upload',
+  });
 
-      <Stat
-        bgColor={'white'}
-        borderRadius={16}
-        p={5}
-        m={5}
-        minW={{ base: '80%', md: '30%' }}
-      >
-        <StatLabel>Gameplay Review</StatLabel>
-        <StatNumber>Operational</StatNumber>
-        <StatHelpText>
-          <Flex
-            flexDirection={'row'}
-            align={'center'}
-            textAlign={'center'}
-            gap={2}
+  return (
+    <>
+      {!aLoading && !rLoading && !uLoading && (
+        <StatGroup>
+          <Stat
+            bgColor={'white'}
+            borderRadius={16}
+            p={5}
+            m={5}
+            minW={{ base: '80%', md: '30%' }}
           >
-            <FaCircle size={8} color={'green'} />
-            <Text>No current issues.</Text>
-          </Flex>
-        </StatHelpText>
-      </Stat>
-    </StatGroup>
+            <StatLabel>Security & Authentication</StatLabel>
+            <StatNumber>
+              {aData?.maintenance ? 'Possible Outage' : 'Operational'}
+            </StatNumber>
+            <StatHelpText>
+              <Flex
+                flexDirection={'row'}
+                align={'center'}
+                textAlign={'center'}
+                gap={2}
+              >
+                <FaCircle
+                  size={8}
+                  color={aData?.maintenance ? 'orange' : 'green'}
+                />
+                <Text>
+                  {aData?.maintenance
+                    ? aData.alertTitle == '--+|[]'
+                      ? 'Maintenance'
+                      : aData.alertTitle
+                    : 'No current issues.'}
+                </Text>
+              </Flex>
+            </StatHelpText>
+          </Stat>
+          <Stat
+            bgColor={'white'}
+            borderRadius={16}
+            p={5}
+            m={5}
+            minW={{ base: '80%', md: '30%' }}
+          >
+            <StatLabel>Data Collection</StatLabel>
+            <StatNumber>
+              {uData?.maintenance ? 'Possible Outage' : 'Operational'}
+            </StatNumber>
+            <StatHelpText>
+              <Flex
+                flexDirection={'row'}
+                align={'center'}
+                textAlign={'center'}
+                gap={2}
+              >
+                <FaCircle
+                  size={8}
+                  color={uData?.maintenance ? 'orange' : 'green'}
+                />
+                <Text>
+                  {uData?.maintenance
+                    ? uData.alertTitle == '--+|[]'
+                      ? 'Maintenance'
+                      : uData.alertTitle
+                    : 'No current issues.'}
+                </Text>
+              </Flex>
+            </StatHelpText>
+          </Stat>
+
+          <Stat
+            bgColor={'white'}
+            borderRadius={16}
+            p={5}
+            m={5}
+            minW={{ base: '80%', md: '30%' }}
+          >
+            <StatLabel>Gameplay Review</StatLabel>
+            <StatNumber>
+              {rData?.maintenance ? 'Possible Outage' : 'Operational'}
+            </StatNumber>
+            <StatHelpText>
+              <Flex
+                flexDirection={'row'}
+                align={'center'}
+                textAlign={'center'}
+                gap={2}
+              >
+                <FaCircle
+                  size={8}
+                  color={rData?.maintenance ? 'orange' : 'green'}
+                />
+                <Text>
+                  {rData?.maintenance
+                    ? rData.alertTitle == '--+|[]'
+                      ? 'Maintenance'
+                      : rData.alertTitle
+                    : 'No current issues.'}
+                </Text>
+              </Flex>
+            </StatHelpText>
+          </Stat>
+        </StatGroup>
+      )}
+    </>
   );
 }
