7b1ec39f1d879e5e5908a78b8dd582e10a8f64ff
ceriddenn <90457772+ceriddenn@users.noreply.github.com>
2022-12-29 18:50:15 -0500

    Update apiHelper.ts

--- apps/web/utils/helpers/apiHelper.ts
+++ apps/web/utils/helpers/apiHelper.ts
@@ -1,37 +1,3 @@
-const api_url = 'http://localhost:4500/';
-const handleUploadFileLogic = async (
-  url: string | undefined,
-  userId: string | number,
-  type: string,
-) => {
-  let result;
-  const options = {
-    method: 'POST',
-    body: JSON.stringify({ id: userId, url: url, type: type }),
-    headers: { 'Content-Type': 'application/json' },
-  };
-  try {
-    const request = await fetch(api_url + 'footage', options);
-    const response = await request.json();
-
-    if (response.error) {
-      result = { message: response.error.message, error: true };
-    } else {
-      result = {
-        message: response.data.message,
-      };
-    }
-  } catch (error) {
-    console.log(error);
-    result = {
-      message:
-        'Sorry, but our server seems to be down. Please try again later.',
-      error: true,
-    };
-  }
-  return result;
-};
-
 const checkURL = (url: string): boolean => {
   const p =
     // eslint-disable-next-line max-len
@@ -42,4 +8,4 @@ const checkURL = (url: string): boolean => {
   return false;
 };
 
-export { handleUploadFileLogic, checkURL };
+export { checkURL };
