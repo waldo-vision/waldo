2a3e2d58adb764fc523da86e87621b8be4fa7967
Husky <39809509+Huskydog9988@users.noreply.github.com>
2023-01-05 16:29:52 -0500

    feat(web): basic opentelemetry support
    only does traces rn

---
+++ apps/web/server/metrics/tracing.ts
@@ -0,0 +1,30 @@
+// Require dependencies
+import * as opentelemetry from '@opentelemetry/sdk-node';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+// import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { PrismaInstrumentation } from '@prisma/instrumentation';
+
+// For troubleshooting, set the log level to DiagLogLevel.DEBUG
+// diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+
+const sdk = new opentelemetry.NodeSDK({
+  traceExporter: new OTLPTraceExporter({
+    // optional - default url is http://localhost:4318/v1/traces
+    url: `${
+      process.env.OTLP_COLLECTOR_BASE || 'http://localhost:4318'
+    }/v1/traces`,
+    // optional - collection of custom headers to be sent with each request, empty by default
+    headers: {},
+    // an optional limit on pending requests
+    concurrencyLimit: 10,
+  }),
+  instrumentations: [
+    // basically does every intrumentation for us, can fine tune later if needed
+    // https://opentelemetry.io/registry/?language=js&component=instrumentation
+    getNodeAutoInstrumentations(),
+    new PrismaInstrumentation(),
+  ],
+});
+
+sdk.start();
