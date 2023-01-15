c5fc99cb79cfb133c36347fdfc903a5325b8d2a3
Husky <39809509+Huskydog9988@users.noreply.github.com>
2023-01-15 17:10:33 -0500

    fix(web): favicon not showing

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -6,6 +6,7 @@ import type { NextPage } from 'next';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import type { ReactElement, ReactNode } from 'react';
+import Head from 'next/head';
 
 export type NextPageWithLayout<P = Record<string, unknown>, IP = P> = NextPage<
   P,
@@ -27,6 +28,22 @@ function App({
 
   return (
     <SessionProvider session={session}>
+      <Head>
+        {/* if the below is uncommented, the favicon breaks */}
+        {/* <link rel="icon" href="favicon.ico" /> */}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+      </Head>
       <ChakraProvider theme={theme}>
         <SiteProvider>{getLayout(<Component {...pageProps} />)}</SiteProvider>
       </ChakraProvider>
