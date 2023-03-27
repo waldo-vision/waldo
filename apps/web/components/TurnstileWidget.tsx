e904eaa908fb44183ad0585c28872469a620c8c0
ceriddenn <ceriddenn@gmail.com>
2023-03-26 20:43:34 -0400

    (hopefully) fixed turnstile

--- apps/web/components/TurnstileWidget.tsx
+++ apps/web/components/TurnstileWidget.tsx
@@ -1,6 +1,5 @@
 import { Box } from '@chakra-ui/react';
 import { Turnstile } from '@marsidev/react-turnstile';
-import { trpc } from '@utils/trpc';
 
 interface WidgetProps {
   valid(valid: boolean, tsToken?: string): void;
@@ -12,22 +11,10 @@ enum CallbackStates {
 }
 
 const TurnstileWidget = (props: WidgetProps) => {
-  const utils = trpc.useContext();
-  const verifyToken = trpc.util.verifyUser.useMutation({
-    async onSuccess() {
-      await utils.util.invalidate();
-    },
-  });
   const handleCallback = async (token: string, state: CallbackStates) => {
     // check for token
     if (state == CallbackStates.SUCCESS) {
-      const result = await verifyToken.mutateAsync({ tsToken: token });
-      if (result.result) {
-        props.valid(true, token);
-      } else {
-        props.valid(false, token);
-      }
-      console.log(result);
+      props.valid(true, token);
     } else if (state == CallbackStates.ERROR) {
       props.valid(false);
     } else if (state == CallbackStates.EXPIRE) {
