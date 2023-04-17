709c9f901e6402ea5797cb3336cc3d308c973efa
Husky <39809509+Huskydog9988@users.noreply.github.com>
2023-04-17 13:06:49 -0400

    fix(sentry): forgot to set sample rate for replays

--- apps/web/sentry.client.config.js
+++ apps/web/sentry.client.config.js
@@ -16,6 +16,14 @@ Sentry.init({
   release: process.env.SENTRY_RELEASE,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 0.5,
+
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
