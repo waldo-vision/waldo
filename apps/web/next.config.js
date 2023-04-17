8691462397f9667d30bb000f212813ca95c5829d
Benedek Major <benedek@major.onl>
2023-04-17 20:21:20 +0200

    fixed double public in cache control
    Signed-off-by: Benedek Major <benedek@major.onl>

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -96,7 +96,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Cache-Control',
-            value: 'public, max-age=9999999999, must-revalidate, public',
+            value: 'public, max-age=9999999999, must-revalidate',
           },
         ],
       },
