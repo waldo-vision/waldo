01b94cf08d8a10a74b81a32022ac0f5508ee701d
Benedek Major <benedek@major.onl>
2023-04-17 20:11:57 +0200

    Added cache control header for static images
    Signed-off-by: Benedek Major <benedek@major.onl>

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -90,6 +90,16 @@ const nextConfig = {
           },
         ],
       },
+      {
+        source: '/:all*(svg|jpg|png)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=9999999999, must-revalidate, public',
+          },
+        ],
+      },
     ];
   },
   async redirects() {
