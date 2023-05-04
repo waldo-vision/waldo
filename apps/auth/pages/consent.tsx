385541804f60f1af04844a60275fa8409893ad9c
Bensikrac <benedek@major.onl>
2023-05-05 01:47:38 +0200

    consent page session verificaton

--- apps/auth/pages/consent.tsx
+++ apps/auth/pages/consent.tsx
@@ -1,4 +1,5 @@
-import { Configuration, OAuth2Api } from '@ory/client';
+import { authError } from '@/lib/utils';
+import { Configuration, FrontendApi, OAuth2Api } from '@ory/client';
 import { NextPageContext } from 'next';
 
 const ory = new OAuth2Api(
@@ -8,6 +9,29 @@ const ory = new OAuth2Api(
 );
 
 export async function getServerSideProps(context: NextPageContext) {
+  //here check if logged in, since no oauth approval if not logged in
+  const cookies = context.req?.headers.cookie;
+  if (cookies == null) {
+    return authError('No cookies set');
+  }
+  const kratos = new FrontendApi( //the built in kratos does not work for some reason
+    new Configuration({
+      basePath: process.env.ORY_SDK_URL,
+    }),
+  );
+  const session = await kratos.toSession(undefined, {
+    headers: { cookie: cookies },
+  });
+  if (session.status != 200) {
+    return authError('Session not valid (!= 200)');
+  }
+  if (!session.data.active) {
+    return authError('Session inactive');
+  }
+
+  const oryid = session.data.id;
+  const email = session.data.identity.traits['email'];
+
   const challenge_text = context.query.consent_challenge?.toString();
   if (challenge_text == null) {
     return;
@@ -23,7 +47,7 @@ export async function getServerSideProps(context: NextPageContext) {
       grant_access_token_audience: challenge.requested_access_token_audience,
       session: {
         id_token: {
-          email: 'TESTEMAIL',
+          email: email,
           image: 'TODOREPLACEIMAGE',
           name: 'TODOREPLACENAME',
           battletag: undefined,
