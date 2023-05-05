6790b8730bb83ae5b7360027cd156ec4f40e969d
Bensikrac <benedek@major.onl>
2023-05-05 18:28:19 +0200

    error handling now has catch

--- apps/auth/pages/error.tsx
+++ apps/auth/pages/error.tsx
@@ -1,11 +1,11 @@
 import { ReactElement } from 'react';
-import { kratos } from '../lib/kratos';
 import { useRouter } from 'next/router';
+import { kratos } from '@/lib/utils';
 
 export default function error(): ReactElement {
   const router = useRouter();
   const errorid = router.query.id as string;
-  const error = kratos.getFlowError({ id: errorid });
+  const error = kratos.getFlowError({ id: errorid }).catch(() => undefined);
 
   return <>{JSON.stringify(error, undefined, 2)}</>;
 }
