0b05c90394c5e7e510164a7532b1348ea3c1b534
Husky <39809509+Huskydog9988@users.noreply.github.com>
2023-05-11 09:33:57 -0400

    test(ui): more button stories

--- packages/ui/components/ui/Button.stories.ts
+++ packages/ui/components/ui/Button.stories.ts
@@ -9,3 +9,23 @@ type Story = StoryObj<typeof Button>;
 export const Primary: Story = {
   args: { variant: 'default', children: 'Hello' },
 };
+
+export const Secoundary: Story = {
+  args: { variant: 'secondary', children: 'Hello' },
+};
+
+export const Destructive: Story = {
+  args: { variant: 'destructive', children: 'Hello' },
+};
+
+export const Outline: Story = {
+  args: { variant: 'outline', children: 'Hello' },
+};
+
+export const Ghost: Story = {
+  args: { variant: 'ghost', children: 'Hello' },
+};
+
+export const Link: Story = {
+  args: { variant: 'link', children: 'Hello' },
+};
--- packages/ui/components/ui/Button.tsx
+++ packages/ui/components/ui/Button.tsx
@@ -8,7 +8,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-red-500',
+        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
