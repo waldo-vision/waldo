373b5b8e0e83bb00e3b95fec674174c43b6366a0
Finn C <ceriddenn@gmail.com>
2023-12-24 16:00:48 -0500

    v1->v2 migration logic done

--- apps/app/pages/api/logto/migration.ts
+++ apps/app/pages/api/logto/migration.ts
@@ -1,14 +1,50 @@
 import { retrieveUserInfoServer } from '@server/utils/logto';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '@server/db/client';
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const logto_user_information = await retrieveUserInfoServer(req.cookies);
+  const logto_user = await retrieveUserInfoServer(req.cookies);
 
-  // check if there is a nextauth user / account with logto user info
+  // check if there is a nextauth account with logto user info
+  // the logto user var isn't typed so use this link for ref
+  // on potential outputs. https://docs.logto.io/docs/references/users/social-identities/
+  try {
+    const result = await prisma?.account.findFirst({
+      where: {
+        providerAccountId:
+          logto_user.userInfo.identities[
+            Object.keys(logto_user.userInfo.identities)[0]
+          ].userId,
+      },
+    });
 
-  // if user / account exists, migrate to logto account.
+    const identityData =
+      logto_user.userInfo.identities[
+        Object.keys(logto_user.userInfo.identities)[0]
+      ].details;
 
-  return res.status(200).json(logto_user_information);
+    // V1 Account exists
+    if (result || result !== null) {
+      try {
+        const createV2Account = await prisma.v2Account.create({
+          data: {
+            provider: Object.keys(logto_user.userInfo.identities)[0],
+            providerAccountId: identityData.id,
+            logtoId: logto_user.claims.sub,
+            userId: result.userId,
+          },
+        });
+      } catch (err) {
+        // handle error
+      }
+    }
+  } catch (err) {
+    // handle error w sentry or something
+  }
+
+  // if no account exists then just create a normal V2 Account.
+
+  return res.status(200).json(logto_user);
 }
