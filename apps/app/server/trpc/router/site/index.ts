6a2026e3f812a6560adc56abb31f3ecd548340f7
Arthur Langley <arthurlangley04@gmail.com>
2024-01-09 23:10:47 +0000

    feat(server/trpc): updatePage to new rbac authentication

--- apps/app/server/trpc/router/site/index.ts
+++ apps/app/server/trpc/router/site/index.ts
@@ -1,7 +1,11 @@
 import { router, protectedProcedure, publicProcedure } from '../../trpc';
 
 import getPageData from './getPageData';
+import getSiteData from './getSiteData';
+import updatePage from './updatePage';
 
 export const siteRouter = router({
-  getPageData
+  getPageData,
+  getSiteData,
+  updatePage,
 });
