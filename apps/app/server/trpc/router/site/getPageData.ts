d5605c9e87b701ea84bf9e2a21d51d05b1df5b9e
Arthur Langley <arthurlangley04@gmail.com>
2024-01-09 22:54:20 +0000

    add(server/trpc): getSiteData to new folder structure

--- apps/app/server/trpc/router/site/getPageData.ts
+++ apps/app/server/trpc/router/site/getPageData.ts
@@ -3,41 +3,39 @@ import { z } from 'zod';
 import { serverSanitize } from '@utils/sanitize';
 import { TRPCError } from '@trpc/server';
 
+const zodInput = z.object({
+    name: z.string(),
+}).transform(input => {
+    return {
+        name: serverSanitize(input.name),
+    };
+});
+
+const zodOutput = z.object({
+    maintenance: z.boolean(),
+    name: z.string(),
+    parentName: z.string(),
+    alertDescription: z.string().nullable(),
+    alertTitle: z.string().nullable(),
+    isCustomAlert: z.boolean(),
+});
+
 export default rbacProtectedProcedure(["read:all", "read:pagemetadata"])
-    .meta({ openapi: { method: 'GET', path: '/site/page' } })
-    .input(
-      z
-        .object({
-          name: z.string(),
-        })
-        .transform(input => {
-          return {
-            name: serverSanitize(input.name),
-          };
-        }),
-    )
-    .output(
-      z.object({
-        maintenance: z.boolean(),
-        name: z.string(),
-        parentName: z.string(),
-        alertDescription: z.string().nullable(),
-        alertTitle: z.string().nullable(),
-        isCustomAlert: z.boolean(),
-      }),
-    )
-    .query(async ({ input, ctx }) => {
-      const pageData = await ctx.prisma.waldoPage.findFirst({
-        where: {
-          name: input.name,
-        },
-      });
-      if (pageData == null) {
-        throw new TRPCError({
-          code: 'NOT_FOUND',
-          message: 'Waldo Vision Page not found in the database.',
-        });
-      }
-      // no error checking because the docs will never be deleted.
-      return pageData;
+.meta({ openapi: { method: 'GET', path: '/site/page' } })
+.input(zodInput)
+.output(zodOutput)
+.query(async ({ input, ctx }) => {
+    const pageData = await ctx.prisma.waldoPage.findFirst({
+    where: {
+        name: input.name,
+    },
     });
+    if (pageData == null) {
+    throw new TRPCError({
+        code: 'NOT_FOUND',
+        message: 'Waldo Vision Page not found in the database.',
+    });
+    }
+    // no error checking because the docs will never be deleted.
+    return pageData;
+});
