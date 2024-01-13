b57435ee458ec0f1292187e0138fc5e4d8b424c8
Finn C <ceriddenn@gmail.com>
2024-01-13 15:52:42 -0500

    fix(env error): put env vars in turbo

--- apps/app/server/trpc/rbac.ts
+++ apps/app/server/trpc/rbac.ts
@@ -1,12 +1,15 @@
 /**
- * 
- * @param userScope 
+ *
+ * @param userScope
  * The current logged on users scope;
- * @param requiredScope 
+ * @param requiredScope
  * The required scope
  * @returns boolean
  */
-export const userHasScope = (userScope: Array<string>, requiredScope: Array<string>): boolean => {
-    const userHasRequiredScope = requiredScope.some(a => userScope.includes(a));
-    return userHasRequiredScope;
-}
+export const userHasScope = (
+  userScope: Array<string>,
+  requiredScope: Array<string>,
+): boolean => {
+  const userHasRequiredScope = requiredScope.some(a => userScope.includes(a));
+  return userHasRequiredScope;
+};
