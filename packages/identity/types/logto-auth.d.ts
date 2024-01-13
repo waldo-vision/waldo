fd3cd600ff8b65ba4edbbf88c3a174ad77585161
Arthur Langley <arthurlangley04@gmail.com>
2024-01-13 18:41:14 +0000

    tweak(packages/identity): update session type and requested scope

--- packages/identity/types/logto-auth.d.ts
+++ packages/identity/types/logto-auth.d.ts
@@ -1,11 +1,14 @@
 interface V2Session {
   logto_id: string;
+  id: string;
   provider: string;
   providerId: string | number;
   name: string;
   image: string;
   logto_username: string;
   blacklisted: boolean;
+  scope: Array<string>;
+  hasScope: (requiredScope: Array<string>) => boolean;
 }
 
 export { V2Session };
