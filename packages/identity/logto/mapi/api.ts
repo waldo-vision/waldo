471f99bc37b9f2b864806ea4ac065a20cc4685da
Finn C <ceriddenn@gmail.com>
2024-01-19 12:59:14 -0500

    feat(mapi/badges): start work on v2 fr's

--- packages/identity/logto/mapi/api.ts
+++ packages/identity/logto/mapi/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import qs from 'qs';
 import { Role } from '../../types/logto-auth';
-const getApiAccessToken = async () => {
+import { LogtoRole } from './types/mapi-type';
+
+const getApiAccessToken = async (): Promise<string> => {
   try {
     const request = await axios.post(
       process.env.MAPI_TOKEN_ENDPOINT,
@@ -23,7 +25,31 @@ const getApiAccessToken = async () => {
     const response = await request.data;
     return response.access_token;
   } catch (error) {
-    return error;
+    throw new Error();
+  }
+};
+
+const getRoleIdByName = async (
+  access_token: string,
+  target: string,
+): Promise<string | undefined> => {
+  try {
+    const request = await axios.get(`${process.env.ENDPOINT}/api/roles`, {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+    const response: Array<LogtoRole> = await request.data;
+
+    for (let i = 0; i < response.length; i++) {
+      if (response[i].name.toLowerCase() == target.toLowerCase()) {
+        return response[i].id;
+      }
+    }
+    return undefined;
+  } catch (error) {
+    throw new Error();
   }
 };
 
@@ -49,4 +75,27 @@ const getUserRoles = async (
   }
 };
 
-export { getApiAccessToken, getUserRoles };
+const assignRoleToUser = async (
+  access_token: string,
+  logtoId: string,
+  target: string,
+): Promise<string> => {
+  const role_id = await getRoleIdByName(access_token, target);
+  try {
+    await axios.post(
+      `${process.env.ENDPOINT}/api/roles/${role_id}/users`,
+      { userIds: [logtoId] },
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      },
+    );
+    return 'Successfull Assignment';
+  } catch (error: any) {
+    return error.response.data.message;
+  }
+};
+
+export { getApiAccessToken, getUserRoles, assignRoleToUser, getRoleIdByName };
